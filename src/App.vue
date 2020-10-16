3e8ddb73473d76b66e0569e31e0145352998bc95
Vishwanath Sastry <vishu.dbz@gmail.com>
2020-10-17 01:42:07 +0530

    Create basic UI for cards

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <CardList />
+    <CardList />
+    <CardList />
+    <CardList />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CardList from "./components/CardList.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+    CardList,
+  },
 };
 </script>
 
 <style>
+body {
+  background: #f6f9fc;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+:root {
+  font-size: 10px;
 }
 </style>
