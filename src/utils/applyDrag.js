d65df8bd9629d8064220fa44193e7440d87f2288
Vishwanath <FrozenHearth@users.noreply.github.com>
2023-01-23 18:02:17 +0530

    Update applyDrag.js

--- src/utils/applyDrag.js
+++ src/utils/applyDrag.js
@@ -1,17 +1,12 @@
-export const applyDrag = (arr, dragResult) => {
-  const { removedIndex, addedIndex, payload } = dragResult;
+export const applyDrag = (arr, { removedIndex, addedIndex, payload }) => {
   if (removedIndex === null && addedIndex === null) return arr;
-
   const result = [...arr];
-  let itemToAdd = payload;
-
+  
   if (removedIndex !== null) {
-    itemToAdd = result.splice(removedIndex, 1)[0];
+    
+    // remove the element at removedIndex and insert it at addedIndex
+    result.splice(addedIndex, 0, result.splice(removedIndex, 1, payload)[0]);
   }
-
-  if (addedIndex !== null) {
-    result.splice(addedIndex, 0, itemToAdd);
-  }
-
+  
   return result;
 };
